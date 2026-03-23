eb41418fe4a7b4fb0167c30021c1897b67ca6e18
Alisalee Aaron <alisalee.aaron@gmail.com>
2026-03-22 20:53:59 -0400

    More accurate citations, more defined process

--- wildfire-desk.py
+++ wildfire-desk.py
@@ -2,6 +2,7 @@ from build.lib.llmproxy import LLMProxy
 import time
 import datetime
 import sys
+from string import Template
 # I advocate for the name Sage for this bot
 #   - its a plant that grows in dry areas such as California which is a huge target for wildfires
 #   - its a name associated with wisdom and protection
@@ -14,6 +15,33 @@ timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 verbose = True
 display_rag = 1
 
+def rag_context_string_simple(rag_context):
+
+    """
+    Convert the RAG context list (from retrieve API)
+    into a single plain-text string that can be appended to a query.
+    """
+
+    context_string = ""
+
+    i=1
+    for collection in rag_context:
+    
+        if not context_string:
+            context_string = """The following is additional context that may be helpful in answering the user's query."""
+
+        context_string += """
+        #{} {}
+        """.format(i, collection['doc_summary'])
+        j=1
+        for chunk in collection['chunks']:
+            context_string+= """
+            #{}.{} {}
+            """.format(i,j, chunk)
+            j+=1
+        i+=1
+    return context_string
+
 ### ----------------------
 ### Sage Settings        -
 ### ----------------------
@@ -23,6 +51,7 @@ sage_core = "" # to be uploaded upon setup
 sage_model = '4o-mini' # subject to change
 sage_temperature = 0.6 # subject to change
 sage_session_id = "sage"+str(timestamp) # subject to change --> may need to save
+sage_RAG_id = "sage_rag"+str(timestamp)
 sage_rag_t = 0.2 # subject to change
 sage_rag_k = 10 # top number of chunks to fetch to use for rag, lets see if we need to set this
 
@@ -31,7 +60,7 @@ example_rag = ""
 def upload_to_sage(filepath):
     response = sage.upload_file(
         file_path = filepath,
-        session_id = sage_session_id,
+        session_id = sage_RAG_id,
         strategy = 'smart' 
     )
     time.sleep(2)
@@ -42,28 +71,40 @@ def upload_to_sage(filepath):
     print("Upload Status: " + resp_message + "\n")
     return resp_message == "success"
 
-def prompt_sage(query_prompt):
+def prompt_sage(query_prompt, include_rag=True):
+    final_query = ""
+    if include_rag:
+        rag_context = sage.retrieve(
+            query = query_prompt,
+            session_id = sage_RAG_id,
+            rag_threshold=sage_rag_t,
+            rag_k=sage_rag_k
+        )
+
+        final_query = Template("$query\n$rag_context").substitute(
+                                query=query_prompt,
+                                rag_context=rag_context_string_simple(rag_context))
+    else:
+        final_query = query_prompt
+
     response = sage.generate(
         model = sage_model,
         system = sage_core,
-        query = query_prompt,
+        query = final_query,
         temperature = sage_temperature,
         session_id = sage_session_id,
-        rag_usage = True, # I cannot see a world where this is not True
-        rag_threshold=sage_rag_t,
-        rag_k=sage_rag_k
+        lastk=3, # the citation proccess usually take three prompts, unsure if this is helpful
     )
+    #     rag_usage = True, # I cannot see a world where this is not True
+    #     rag_threshold=sage_rag_t,
+    #     rag_k=sage_rag_k
+    # )
 
-    context = sage.retrieve(
-        query = query_prompt,
-        session_id = sage_session_id,
-        rag_threshold=sage_rag_t,
-        rag_k=sage_rag_k
-    )
+    
 
-    print(f"""\n!!!!!!!!!!!!!!!!!!!!!!!\nretrieved: {context}\n!!!!!!!!!!!!!!!!!!!!!!!\n""")
+    # print(f"""\n!!!!!!!!!!!!!!!!!!!!!!!\nretrieved: {rag_context}\n!!!!!!!!!!!!!!!!!!!!!!!\n""")
 
-    return response
+    return response, rag_context
 
 # returns if this was successful or not
 def setup_sage():
@@ -108,12 +149,6 @@ introduction_prompt = "Introduce yourself and say what it is you can do for the 
 ### -----------------------
 
 def get_source(file, rag_context):
-    source_prompt = f"""What documents are referenced in this Rag Context?<start> {rag_context} <end>
-    
-    If there is only empty space between <start> and <end> reply with three empty spaces.
-
-    Otherwise, make sure your answer follows the format: **[Document Name]**\n - [Summary]\n - [Key sections referenced]
-    """
     # Here is an example of that format:
     # 1. **"Ella Taught Me: Shattering the Myth of the Leaderless Movement" - Colorlines**
     # - This document addresses the misconception of leaderless movements, emphasizing the importance of organized leadership and collective strategy within social movements. It references historical examples such as the Student Nonviolent Coordinating Committee (SNCC) and the Black Panther Party, highlighting the concept of group-centered leadership as essential for accountability and effective mobilization.
@@ -121,34 +156,88 @@ def get_source(file, rag_context):
     #     - **Group-Centered Leadership**: Discusses the necessity for structured organizations like the Black Youth Project 100 (BYP100) and their approach to activism.
     #     - **Collective Strategy**: Emphasizes the need for organizations to ensure accountability and coordinated efforts among activists.
     #     - **Experiences in Movements**: Reflects on the author's experiences balancing mobilization with organization-building and the importance of communication.
+    source_prompt = f"""What documents are referenced in the doc_summary sections of this Rag Context?<start> {rag_context} <end>
     
+    If there is only empty space between <start> and <end> reply with three empty spaces.
 
-    response = prompt_sage(source_prompt)
-    log_sage(file, response)
+    Otherwise, make sure your answer follows the format: **[Document Name]**\n - [Summary]\n - [Key sections referenced]
+    """
+    
+    response, _ = prompt_sage(source_prompt)
+    # log_sage(file, response, "irrelevant 1")
 
-    citation_prompt = f"""Here is some information on documents Sage referenced: <start> {response["result"]}<end>\n  
-    If there is only empty space between <start> and <end> then respond by saying 'No resources were referenced to make the above response'.
-    Otherwise, give a citation for each resource mentioned between <start> and <end>. 
-    Respond with only citations in MLA format, if you are not able to fill in a portion of the citation then omit it. Do not write duplicate citations. Do not use citations from the reference portion of the document, only cite the document itself."""
-    response = prompt_sage(citation_prompt)
+    # doc_data = ""
+    # if isinstance(response, dict):
+    #     doc_data = response["result"]
+    # elif isinstance(response, tuple):
+    #     doc_data = response[0]["result"]
+    # else:
+    #     doc_data = response
+
+    # print(f"""\nDoc data: {doc_data}\n""")
+
+    doc_summaries, number = parse_retrieve_rag_context(rag_context)
+    # print(f"""Doc Summaires: {doc_summaries}\n""")
+
+    # citation_prompt = f"""Here are the summaries of each document Sage referened in the previous prompt: <start> {doc_summaries}<end>\n  
+    # If there is only empty space between <start> and <end> then respond by saying 'No resources were referenced to make the above response'.
+    # Otherwise, If there is information it is in the format **[Document Name]**\n - [Summary]\n - [Key sections referenced]
+    # Give a citation for each document mentioned between <start> and <end>. 
+    # Respond only with citations in MAL format. If you are not able to fill in a portion of the citation then omit it. Do not write duplicate citations. Do not use citations from the reference portion of the document, only cite the document itself."""
+
+    citation_prompt = f""" Give MLA format citations for each document referenced below:\n {doc_summaries}\n 
+    If you are not able to fill in a portion of the citation then omit it. Only return the top {number} relevant citation(s)."""
+    # There should be one citation per document referenced.
+    # citation_prompt = f"""Here is some information on documents Sage referenced: <start> {doc_data}<end>\n  
+    # If there is only empty space between <start> and <end> then respond by saying 'No resources were referenced to make the above response'.
+    # Otherwise, If there is information it is in the format **[Document Name]**\n - [Summary]\n - [Key sections referenced]
+    # Give a citation for each document mentioned between <start> and <end>. 
+    # Respond only with citations in MAL format. If you are not able to fill in a portion of the citation then omit it. Do not write duplicate citations. Do not use citations from the reference portion of the document, only cite the document itself."""
+    response, ctx = prompt_sage(citation_prompt)
     # log_sage(file, response)
-    return response
+    return response, ctx
     
+def parse_retrieve_rag_context(rag_ctx):
+    # the rag cotext from retrieve() is a list of dictionaries with the keys: doc_id, doc_summary and chunks.
+    summaries = ""
+    index = 1
+    for rec in rag_ctx:
+        summaries += f"""{str(index)}. {rec["doc_summary"]}\n"""
+        index+=1
+    
+    return summaries, index
+
 def log_user(file, text, verbose=verbose):
     phrase = "Anon: " + text + "\n\n"
     file.write(phrase)
     if(verbose):
         print(phrase)
 
-def log_sage(file, response, verbose=verbose, display_rag=display_rag):
-    phrase = "Sage: " + response["result"] + "\n"
+def log_sage(file, response, rag_context, verbose=verbose, display_rag=display_rag):
+    phrase = ""
+    print(f"""Type: {type(response)}\n""")
+    if isinstance(response, dict):
+        phrase = f"""Sage: {response.get("result")}\n"""
+    elif isinstance(response, tuple):
+        phrase = f"""Sage: {response[0]["result"]}\n"""
+    else:
+        print("The Else")
+        phrase = f"""Sage: {response}\n"""
+    # phrase = "Sage: " + response["result"] + "\n"
     file.write(phrase)
+    # if(verbose):
+    #     print(phrase)
+    # if(display_rag == 1):
+    #     print(f"""\n******************\nRag_context: {response["rag_context"]} \nRag_context_length: {len(response["rag_context"])} \n******************\n\n""")
+    # elif(display_rag == 2):
+    #     print(f"""\n******************\nRag_context_length: {len(response["rag_context"])} \n******************\n\n""")
+
     if(verbose):
         print(phrase)
     if(display_rag == 1):
-        print(f"""\n******************\nRag_context: {response["rag_context"]} \nRag_context_length: {len(response["rag_context"])} \n******************\n\n""")
+        print(f"""\n******************\nRag_context: {rag_context} \nRag_context_length: {len(rag_context)} \n******************\n\n""")
     elif(display_rag == 2):
-        print(f"""\n******************\nRag_context_length: {len(response["rag_context"])} \n******************\n\n""")
+        print(f"""\n******************\nRag_context_length: {len(rag_context)} \n******************\n\n""")
 
 
 def assess_question_type(file, text):
@@ -164,17 +253,18 @@ def main():
     #log collection with debugging
     with open(f"""log-{timestamp}.txt""", "w", encoding="utf-8") as file:
         resp = prompt_sage(introduction_prompt)
-        log_sage(file, resp)
+        log_sage(file, resp, "")
         usr = input("Type your response here: ")
         # log_user(file, usr)
         with open("sage-resources/example-rag.txt") as f:
             global example_rag
             example_rag = f.read()
         while usr != "quit":
-            resp = prompt_sage(usr)
-            log_sage(file, resp)
-            citations = get_source(file, resp["rag_context"])
-            log_sage(file, citations)
+            resp, rag_context = prompt_sage(usr)
+            log_sage(file, resp, rag_context)
+            if len(rag_context) > 0:
+                citations, rag_context = get_source(file, rag_context)
+                log_sage(file, citations, rag_context)
 
             # get_source(file, example_rag)
             usr = input("Type your response here: ")
@@ -185,19 +275,6 @@ if __name__ == '__main__':
 
 
 # Example tester questions for citations
-# What is action 109 of nonviolent actions
-# How could i lead a leaderless movement and tell me how that would relate to nonviolent action 106
-# In reference to nonviolent action 106, how would I run a leaderless movement to acomplish that non-violent action
-# In reference to nonviolent action 106, how would I run a leaderless movement to acomplish that non-violent action and how would I develop my network in this highly connected world?
 # why do network strateigies deserve our attention?
 # how would a leaderless movement do 198 methods of nonviolent action?
-
-
-# Doc ids
-# 9eac9c1d32fca5fc8439a79f97582ede56b4931095bd98f37bc99f00705fe2f7
-# a3fb96284ccdf694077f279689b85c650e21f79256f61acd560c1fd5c61fa41b
-# a3fb96284ccdf694077f279689b85c650e21f79256f61acd560c1fd5c61fa41b
-# 9a78d8df722135e34daaa5ea9912e42dbd5b5a0565ff845282001d75df58b64c
-# 9a78d8df722135e34daaa5ea9912e42dbd5b5a0565ff845282001d75df58b64c
-# 9a78d8df722135e34daaa5ea9912e42dbd5b5a0565ff845282001d75df58b64c
-# 9a78d8df722135e34daaa5ea9912e42dbd5b5a0565ff845282001d75df58b64c
+# what is the nonviolent method number 31?
