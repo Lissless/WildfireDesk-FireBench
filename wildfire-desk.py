bb5abb1e8e7d3ac40f9ff5ec1efca143667040a5
Alisalee Aaron <alisalee.aaron@gmail.com>
2026-03-22 22:08:25 -0400

    Add all valid resources, cleanup

--- wildfire-desk.py
+++ wildfire-desk.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import sys
 from string import Template
+from pathlib import Path
 # I advocate for the name Sage for this bot
 #   - its a plant that grows in dry areas such as California which is a huge target for wildfires
 #   - its a name associated with wisdom and protection
@@ -13,20 +14,17 @@ from string import Template
 
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 verbose = True
-display_rag = 1
+display_rag = 0
+chatbot_resources_directory = "sage-resources/democracy-chatbot-resources"
 
 def rag_context_string_simple(rag_context):
-
     """
     Convert the RAG context list (from retrieve API)
     into a single plain-text string that can be appended to a query.
     """
-
     context_string = ""
-
     i=1
     for collection in rag_context:
-    
         if not context_string:
             context_string = """The following is additional context that may be helpful in answering the user's query."""
 
@@ -52,8 +50,8 @@ sage_model = '4o-mini' # subject to change
 sage_temperature = 0.6 # subject to change
 sage_session_id = "sage"+str(timestamp) # subject to change --> may need to save
 sage_RAG_id = "sage_rag"+str(timestamp)
-sage_rag_t = 0.2 # subject to change
-sage_rag_k = 10 # top number of chunks to fetch to use for rag, lets see if we need to set this
+sage_rag_t = 0.4 # subject to change
+sage_rag_k = 5 # top number of chunks to fetch to use for rag, lets see if we need to set this
 
 example_rag = ""
 
@@ -63,12 +61,13 @@ def upload_to_sage(filepath):
         session_id = sage_RAG_id,
         strategy = 'smart' 
     )
-    time.sleep(2)
+    time.sleep(3)
     if "result" not in response:
         print("Resource upload error\n")
         return False
     resp_message = response["result"]
-    print("Upload Status: " + resp_message + "\n")
+    if verbose:
+        print("Upload Status: " + resp_message)
     return resp_message == "success"
 
 def prompt_sage(query_prompt, include_rag=True):
@@ -95,28 +94,23 @@ def prompt_sage(query_prompt, include_rag=True):
         session_id = sage_session_id,
         lastk=3, # the citation proccess usually take three prompts, unsure if this is helpful
     )
-    #     rag_usage = True, # I cannot see a world where this is not True
-    #     rag_threshold=sage_rag_t,
-    #     rag_k=sage_rag_k
-    # )
-
-    
-
-    # print(f"""\n!!!!!!!!!!!!!!!!!!!!!!!\nretrieved: {rag_context}\n!!!!!!!!!!!!!!!!!!!!!!!\n""")
 
     return response, rag_context
 
 # returns if this was successful or not
 def setup_sage():
     # upload and store the string contents of the tone 
-    if not upload_to_sage("sage-resources/question-types.pdf"):
-        return False
-    if not upload_to_sage("sage-resources/democracy-chatbot-resources/strategy-and-organizing/198 Methods of Nonviolent Action — AEI_ Empowering Humankind.pdf"):
-        return False
-    if not upload_to_sage("sage-resources/democracy-chatbot-resources/strategy-and-organizing/Ella Taught Me: Shattering the Myth of the Leaderless Movement - Colorlines.pdf"):
-        return False
-    if not upload_to_sage("sage-resources/democracy-chatbot-resources/strategy-and-organizing/Leading with Network Mindset .pdf"):
-        return False
+    # if not upload_to_sage("sage-resources/question-types.pdf"):
+    #     return False
+    p = Path(chatbot_resources_directory)
+    for dir in p.iterdir():
+        if dir.is_dir():
+            for file in dir.iterdir():
+                # print("Name: ", file.name, "Relative: ", file.relative_to("."), "\n")
+                if not upload_to_sage(file.relative_to(".")):
+                    print(f"""File: {file.name} - failed to upload\n""")
+                    return False
+            # print(f.read_text())
     try:
         with open("sage-resources/sage-tone.txt") as f:
             global sage_core
@@ -129,13 +123,13 @@ def setup_sage():
 ### Soil Settings        -
 ### ----------------------
 
-sage = LLMProxy()
-sage_core = "" # to be uploaded upon setup
-sage_model = '4o-mini' # subject to change
-sage_temperature = 0.6 # subject to change
-sage_session_id = "sage"+str(timestamp) # subject to change --> may need to save
-sage_rag_t = 0.4 # subject to change
-sage_rag_k = 5 # top number of documents to fetch to use for rag, lets see if we need to set this
+# sage = LLMProxy()
+# sage_core = "" # to be uploaded upon setup
+# sage_model = '4o-mini' # subject to change
+# sage_temperature = 0.6 # subject to change
+# sage_session_id = "sage"+str(timestamp) # subject to change --> may need to save
+# sage_rag_t = 0.4 # subject to change
+# sage_rag_k = 5 # top number of documents to fetch to use for rag, lets see if we need to set this
 
 
 ### -----------------------
@@ -163,38 +157,13 @@ def get_source(file, rag_context):
     Otherwise, make sure your answer follows the format: **[Document Name]**\n - [Summary]\n - [Key sections referenced]
     """
     
-    response, _ = prompt_sage(source_prompt)
-    # log_sage(file, response, "irrelevant 1")
-
-    # doc_data = ""
-    # if isinstance(response, dict):
-    #     doc_data = response["result"]
-    # elif isinstance(response, tuple):
-    #     doc_data = response[0]["result"]
-    # else:
-    #     doc_data = response
-
-    # print(f"""\nDoc data: {doc_data}\n""")
+    response, _ = prompt_sage(source_prompt) # TODO: Test if this is needed, this prompt primes the next one
 
     doc_summaries, number = parse_retrieve_rag_context(rag_context)
-    # print(f"""Doc Summaires: {doc_summaries}\n""")
-
-    # citation_prompt = f"""Here are the summaries of each document Sage referened in the previous prompt: <start> {doc_summaries}<end>\n  
-    # If there is only empty space between <start> and <end> then respond by saying 'No resources were referenced to make the above response'.
-    # Otherwise, If there is information it is in the format **[Document Name]**\n - [Summary]\n - [Key sections referenced]
-    # Give a citation for each document mentioned between <start> and <end>. 
-    # Respond only with citations in MAL format. If you are not able to fill in a portion of the citation then omit it. Do not write duplicate citations. Do not use citations from the reference portion of the document, only cite the document itself."""
 
     citation_prompt = f""" Give MLA format citations for each document referenced below:\n {doc_summaries}\n 
     If you are not able to fill in a portion of the citation then omit it. Only return the top {number} relevant citation(s)."""
-    # There should be one citation per document referenced.
-    # citation_prompt = f"""Here is some information on documents Sage referenced: <start> {doc_data}<end>\n  
-    # If there is only empty space between <start> and <end> then respond by saying 'No resources were referenced to make the above response'.
-    # Otherwise, If there is information it is in the format **[Document Name]**\n - [Summary]\n - [Key sections referenced]
-    # Give a citation for each document mentioned between <start> and <end>. 
-    # Respond only with citations in MAL format. If you are not able to fill in a portion of the citation then omit it. Do not write duplicate citations. Do not use citations from the reference portion of the document, only cite the document itself."""
     response, ctx = prompt_sage(citation_prompt)
-    # log_sage(file, response)
     return response, ctx
     
 def parse_retrieve_rag_context(rag_ctx):
@@ -215,23 +184,13 @@ def log_user(file, text, verbose=verbose):
 
 def log_sage(file, response, rag_context, verbose=verbose, display_rag=display_rag):
     phrase = ""
-    print(f"""Type: {type(response)}\n""")
     if isinstance(response, dict):
         phrase = f"""Sage: {response.get("result")}\n"""
     elif isinstance(response, tuple):
         phrase = f"""Sage: {response[0]["result"]}\n"""
     else:
-        print("The Else")
         phrase = f"""Sage: {response}\n"""
-    # phrase = "Sage: " + response["result"] + "\n"
     file.write(phrase)
-    # if(verbose):
-    #     print(phrase)
-    # if(display_rag == 1):
-    #     print(f"""\n******************\nRag_context: {response["rag_context"]} \nRag_context_length: {len(response["rag_context"])} \n******************\n\n""")
-    # elif(display_rag == 2):
-    #     print(f"""\n******************\nRag_context_length: {len(response["rag_context"])} \n******************\n\n""")
-
     if(verbose):
         print(phrase)
     if(display_rag == 1):
@@ -263,8 +222,11 @@ def main():
             resp, rag_context = prompt_sage(usr)
             log_sage(file, resp, rag_context)
             if len(rag_context) > 0:
+                print("Citations: ")
                 citations, rag_context = get_source(file, rag_context)
                 log_sage(file, citations, rag_context)
+            else:
+                print("WARNING: No vetted resoures were used to produce the information above")
 
             # get_source(file, example_rag)
             usr = input("Type your response here: ")
